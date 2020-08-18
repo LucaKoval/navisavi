e661e3cc870edb3e4c108fa2fbc013ac36eeeeec
Luca Koval <lucakoval@me.com>
2020-08-18 14:26:29 -0400

    Trying config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,18 +72,18 @@ module.exports = {
         pageTransitionDelay: 0,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/favicon.png`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     icon: `src/favicon.png`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-offline`,
       options: {
