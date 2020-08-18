0687ea5253587f11cf953933852146d3d1423715
Luca Koval <lucakoval@me.com>
2020-08-18 14:32:23 -0400

    Trying more config fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,20 +84,20 @@ module.exports = {
     //     icon: `src/favicon.png`,
     //   },
     // },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [
-          `/404/`,
-          `/about/`,
-          `/contact/`,
-          `/careers/`,
-          `/earn/`,
-          `/privacy-policy/`,
-          `/terms-of-service/`,
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [
+    //       `/404/`,
+    //       `/about/`,
+    //       `/contact/`,
+    //       `/careers/`,
+    //       `/earn/`,
+    //       `/privacy-policy/`,
+    //       `/terms-of-service/`,
+    //     ],
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
