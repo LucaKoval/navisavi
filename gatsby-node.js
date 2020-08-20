f9687d7df81f192a9e1fd44869b7917028a21d66
Luca Koval <lucakoval@me.com>
2020-08-20 15:48:15 -0400

    Fixing blog issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,56 +2,6 @@ const path = require("path")
 const careersInfo = require("./src/data/careersInfo")
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.js")
-    resolve(
-      graphql(`
-        {
-          allContentfulBlog(sort: { fields: date, order: ASC }) {
-            edges {
-              node {
-                body {
-                  body
-                  childMarkdownRemark {
-                    html
-                  }
-                }
-                author
-                id
-                date(formatString: "DD MMM, YYYY")
-                slug
-                title
-                image {
-                  fluid {
-                    src
-                    srcSet
-                  }
-                }
-              }
-            }
-          }
-        }
-      `).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
-        result.data.allContentfulBlog.edges.forEach(edge => {
-          createPage({
-            path: `blog/${edge.node.slug}`,
-            component: blogPostTemplate,
-            context: {
-              slug: edge.node.slug,
-            },
-          })
-        })
-        return
-      })
-    )
-  })
-}
-
-exports.createPages = ({ actions }) => {
 	const { createPage } = actions
 	const careerDescriptionTemplate = path.resolve("src/templates/careerDescriptionTemplate.js")
 	careersInfo.careers.forEach(career => {
@@ -63,5 +13,51 @@ exports.createPages = ({ actions }) => {
         	},
       	})
     })
-    return
+
+ 	return new Promise((resolve, reject) => {
+	    const blogPostTemplate = path.resolve("src/templates/blogPostTemplate.js")
+	    resolve(
+	        graphql(`
+	            {
+	                allContentfulBlog(sort: { fields: date, order: ASC }) {
+	                    edges {
+	                        node {
+	                            body {
+	                                body
+	                                childMarkdownRemark {
+	                                    html
+	                                }
+	                            }
+	                            author
+	                            id
+	                            date(formatString: "DD MMM, YYYY")
+	                            slug
+	                            title
+	                            image {
+	                                fluid {
+	                                    src
+	                                    srcSet
+	                                }
+	                            }
+	                        }
+	                    }
+	                }
+	            }  
+	        `).then(result => {
+	            if (result.errors) {
+	                reject(result.errors)
+	            }
+	            result.data.allContentfulBlog.edges.forEach(edge => {
+	                createPage({
+		                path: `blog/${edge.node.slug}`,
+		                component: blogPostTemplate,
+		                context: {
+		                  	slug: edge.node.slug,
+		                },
+	              	})
+	            })
+	            return
+	        })
+	    )
+  	})
 }
