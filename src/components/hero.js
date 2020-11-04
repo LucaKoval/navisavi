84aee36185f3d71e6b7ad8eb676062305a6fbf40
Luca Koval <lucakoval@me.com>
2020-11-04 15:30:43 -0500

    Adding sign up modal

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import HomeVideoMP4 from "../videos/final.mp4"
 import styles from "../styles/Hero.module.css"
+import { GrFormClose } from "react-icons/gr"
 
 const Hero = () => {
+    const [displaySignupModal, setDisplaySignupModal] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (typeof document !== "undefined") document.body.classList.add('modal-open')
+            setDisplaySignupModal(true)
+        }, 10000)
+    }, [])
+
+    const closeModal = () => {
+        setDisplaySignupModal(false)
+        if (typeof document !== "undefined") document.body.className = document.body.className.replace('modal-open','')
+    }
+
     const data = useStaticQuery(graphql`
       {
         site {
@@ -33,6 +48,39 @@ const Hero = () => {
     `)
     return (
         <div>
+            { displaySignupModal &&
+                <div className={styles.signupModalContainer}>
+                    <div className={styles.signupModal}>
+                        <GrFormClose className={styles.closeModal} onClick={closeModal}/>
+                        <div className={styles.tagline}><div>Sign Up</div></div>
+                        <div className={styles.signupModalTagline}>
+                            Sign up now to be the first to know about NaviSavi and VIP access! Coming soon!
+                        </div>
+                        <div className={styles.inputContainer}>
+                            <form
+                                name="beta-signup"
+                                method="POST"
+                                data-netlify="true"
+                                className={styles.form}
+                            >
+                            <input
+                                id="beta-email"
+                                type="email"
+                                name="beta-email"
+                                className=""
+                                placeholder="Email"
+                                className={styles.mainInput}
+                            />
+                            <input type="hidden" name="form-name" value="beta-signup" />
+                            <div className={styles.buttonContainer}>
+                                <div className={styles.buttonShadow} />
+                                <input type="submit" value="Join"/>
+                            </div>
+                          </form>
+                        </div>
+                    </div>
+                </div>
+            }
             <div className={styles.container}>
                 <div className={`${styles.leftContainer} ${styles.mainElements}`}>
                     <div className={styles.content}>
