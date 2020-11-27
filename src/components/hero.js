7127b49ce3af6dff1821daa444964b75e4ebd3b3
Luca Koval <lucakoval@me.com>
2020-11-27 11:36:44 -0500

    Fixing form submission on reload of contact page

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
     useEffect(() => {
         if (typeof window !== "undefined") {
             if (window.history.replaceState) {
-                window.history.replaceState(null, null, window.location.href);
+                window.history.replaceState(null, null, window.location.href)
             }
         }
 
