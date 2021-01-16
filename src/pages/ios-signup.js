ea32ed8b5c53ac1c942ceb10eaf1257ba9b6df37
Luca Koval <lucakoval@me.com>
2021-01-16 16:32:03 -0500

    Simplifying signups (returning)

--- src/pages/ios-signup.js
+++ src/pages/ios-signup.js
@@ -1,71 +1,14 @@
 import React from "react"
-import * as Yup from "yup"
-import { Formik } from "formik"
 
-import signupStyles from "../styles/Signup.module.css"
-import styles from "../styles/Hero.module.css"
-
-const signInValidationSchema = Yup.object().shape({
- 	betaEmail: Yup.string()
-    	.label("Email")
-    	.email("Please enter a valid email")
-    	.required("Please enter an email")
-});
+import Signup from "../components/signup.js"
+import styles from "../styles/Signup.module.css"
 
 const IOSSignup = () => {
     return (
-    	<div className={signupStyles.container}>
-    		<div className={signupStyles.formContainer}>
-    			<div className={signupStyles.title}>Get the app early!</div>
-    			<Formik
-    			    initialValues={{ betaEmail: '' }}
-    			    validationSchema={signInValidationSchema}
-    			>
-    			    {({
-    			        values,
-    			        errors,
-    			        touched,
-    			        handleChange,
-    			        handleBlur,
-    			        dirty,
-    			        isValid
-    			    }) => (
-						<form
-						    name="beta-signup-ios"
-						    method="POST"
-						    data-netlify="true"
-						    className={styles.form}
-						    action="/"
-						> 
-						    <div className={styles.inputContainer}>
-						        <div className={styles.inputField}>
-						            <input
-						                id="betaEmail"
-						                type="email"
-						                name="betaEmail"
-						                onChange={handleChange}
-							            onBlur={handleBlur}
-										value={values.betaEmail}
-						                placeholder="Email (required)"
-						                aria-required="true"
-						            />
-						            <label htmlFor="betaEmail">Email (required)</label>
-						        </div>
-						        { (errors.betaEmail && touched.betaEmail) &&
-						        	<div className={styles.error} style={ (errors.betaEmail && touched.betaEmail) ? {} : { display: 'none' }}>
-						        		{errors.betaEmail}
-						        	</div>
-						        }
-						    </div>
-						    <input type="hidden" name="form-name" value="beta-signup-ios" />
-						    <div className={styles.buttonContainer}>
-						    	{ !(isValid && dirty) && <div className={styles.buttonDisabled} /> }
-						        <div className={styles.buttonShadow} />
-						        <input type="submit" value="Sign Up" disabled={!(isValid && dirty)} />
-						    </div>
-						</form>
-					)}
-				</Formik>
+    	<div className={styles.container}>
+    		<div className={styles.formContainer}>
+    			<div className={styles.title}>Get the app early!</div>
+    			<Signup formName="beta-signup-ios" />
     		</div>
     	</div>
     )
