8234f2f591e0d5f0b4975a49be3cc8b4fa98bb58
Luca Koval <lucakoval@me.com>
2021-01-16 00:26:03 -0500

    Cleaning header

--- src/components/header.js
+++ src/components/header.js
@@ -112,9 +112,6 @@ export default class Header extends Component {
 			    	<div className={styles.darkenBackground} style={ navOpen ? {} : { display: "none" } } />
 			    	<div className={`${styles.hamburgerMenu} ${navOpen ? "" : styles.hamburgerMenuClose}`}>
 				    	<div className={styles.hamburgerMenuItemContainer}>
-				    		<a href="/#features" className={styles.hamburgerMenuItem} onClick={() => this.toggleNav()}>Features</a>
-				    	</div>
-				    	<div className={styles.hamburgerMenuItemContainer}>
 					    	<Link to="/earn" className={styles.hamburgerMenuItem} onClick={() => this.toggleNav()}>
 					    		Rewards
 					    	</Link>
@@ -172,9 +169,6 @@ export default class Header extends Component {
 			    	</div>
 		    	</div>
 		    	<div className={styles.navRight} style={ displayDownloadButton() ? { marginTop: -2, top: 9, right: 45 } : { marginTop: 8, bottom: 0, right: 12 }}>
-			    	<div className={styles.navItem}>
-			    		<a href="/#features" className={styles.navLink}>Features</a>
-			    	</div>
 			    	<div className={styles.navItem}>
 				    	<Link to="/earn" className={styles.navLink}>
 				    		Rewards
